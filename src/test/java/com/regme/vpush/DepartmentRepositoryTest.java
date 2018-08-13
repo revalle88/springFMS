b19bf8c5194351e7a5b92036f96035fe94ad1125
revalle88 <revalle88@gmail.com>
2018-08-13 17:12:10 +0300

    Try Unit Tests

---
+++ src/test/java/com/regme/vpush/DepartmentRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.regme.vpush;
+
+import com.regme.vpush.domain.Department;
+import com.regme.vpush.repository.DepartmentRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/**
+ * Created by admin on 13.08.2018.
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class DepartmentRepositoryTest {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+    @Test
+    public void whenFindByCode_thenReturnDepartment() {
+       // String code = "440011";
+        Department department = new Department("fmsTestName", "440011");
+        this.departmentRepository.save(department);
+        Department foundDepartment = departmentRepository.findByCode("440011");
+        assertThat(foundDepartment).isEqualTo(department);
+    }
+}
---
+++ src/test/java/com/regme/vpush/FmsControllerTest.java
@@ -0,0 +1,38 @@
+package com.regme.vpush;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by admin on 13.08.2018.
+ */
+public class FmsControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+
+    public void validate_customerInfo1() throws Exception {
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/test");
+        mockMvc.perform(request).andExpect(status().isOk());
+    }
+}
