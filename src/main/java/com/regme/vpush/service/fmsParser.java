cf19ea13bc3de287958df60c5b99db38d38383d7
revalle88 <revalle88@gmail.com>
2018-08-13 10:45:27 +0300

    FMSParser Service

---
+++ src/main/java/com/regme/vpush/service/fmsParser.java
@@ -0,0 +1,10 @@
+package com.regme.vpush.service;
+
+/**
+ * Created by admin on 13.08.2018.
+ */
+public interface fmsParser {
+   // String parse();
+  //  String unzip();
+   String load();
+}
---
+++ src/main/java/com/regme/vpush/service/fmsParserImpl.java
@@ -0,0 +1,32 @@
+package com.regme.vpush.service;
+
+import java.io.File;
+import java.net.URL;
+import org.apache.commons.io.*;
+
+/**
+ * Created by admin on 13.08.2018.
+ */
+public class fmsParserImpl implements fmsParser {
+    private File zipFile;
+    private String downloadDir = ClassLoader.getSystemResource(".").getFile();
+    private String zipFileName = "fms.zip";
+    private String sourceURL = "http://webzato.com/fms/fms_structure_10012018.zip";
+    public String load(){
+
+
+        try {
+            System.out.println(ClassLoader.getSystemResource("."));
+            //TODO project resources
+            zipFile = new File(downloadDir + zipFileName);
+            if (zipFile.createNewFile()) {
+                System.out.println("File is created!");
+            } else {
+                System.out.println("File already exists.");
+            }           
+            FileUtils.copyURLToFile(new URL(sourceURL), zipFile);
+        }
+        catch (Exception e) { e.printStackTrace(); }
+        return "OK";
+    }
+}
